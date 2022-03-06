b070064bb58f6a4e25e2095552e186d8c13f422b
gimnathperera <gimnath.perera@secureagility.com>
2022-03-06 22:34:29 +0530

    [FIX]  add worker remove mechanism

--- src/content/applications/Jobs/AddWorkerToJobForm.tsx
+++ src/content/applications/Jobs/AddWorkerToJobForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
 import { Formik, Form, FieldArray } from 'formik';
 import * as Yup from 'yup';
